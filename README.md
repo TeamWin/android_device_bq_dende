8096b8c2c36c956d27006bd12829f18b63b501f9
Kra1o5 <kra1o5x@gmail.com>
2015-10-14 18:31:29 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+TWRP device tree for the BQ Aquaris M4.5, aka dende
+========================================================
+
+For building TWRP only.
---
+++ omni_dende.mk
@@ -0,0 +1,9 @@
+# Inherit device configuration
+$(call inherit-product, device/bq/dende/device.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := dende
+PRODUCT_NAME := omni_dende
+PRODUCT_BRAND := bq
+PRODUCT_MANUFACTURER := bq
+PRODUCT_MODEL := Aquaris M4.5
